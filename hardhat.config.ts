bad6c18f97f0718a7a748879ed1ba470a5c5cc9c
pde-rent <paul@astrolab.fi>
2024-02-28 21:12:48 +0100

    divide and conquer

---
+++ hardhat.config.ts
@@ -0,0 +1,34 @@
+// TODO: update to ethers v6+@nomicfoundation once tenderly supports it
+import "@nomiclabs/hardhat-ethers";
+import "@nomiclabs/hardhat-etherscan";
+
+import { config } from "@astrolabs/hardhat/dist/hardhat.config";
+
+config.solidity!.compilers = [
+  {
+    version: "0.8.22",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 1000,
+      },
+      viaIR: false,
+      evmVersion: `paris`
+    },
+  },
+];
+
+config.paths = {
+  // registry
+  interfaces: "../registry/interfaces",
+  abis: "../registry/abis",
+  registry: "../registry",
+  // tmp build files
+  artifacts: "./artifacts",
+  cache: "./cache",
+  // local sources
+  sources: "./src",
+  tests: "./test/integration",
+} as any;
+
+export default config;
---
+++ src/ERC20MultiTransferOwnable.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import { ERC20MultiTransfer } from "./ERC20MultiTransfer.sol";
+import { Ownable } from "solady/src/auth/Ownable.sol";
+
+/// @title ERC20MultiTransferOwnable
+/// @author AstrolabDAO (https://github.com/AstrolabDAO/erc20-multitransfer/blob/main/src/ERC20MultiTransfer.sol)
+contract ERC20MultiTransferOwnable is ERC20MultiTransfer, Ownable {
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*               ERC20 METADATA AND CONSTRUCTOR               */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    // Add state variables for name, symbol, and decimals
+    string public name_;
+    string public symbol_;
+    uint8 immutable public decimals_;
+
+    constructor(string memory _name, string memory _symbol, uint8 _decimals, address _owner) {
+        name_ = _name;
+        symbol_ = _symbol;
+        decimals_ = _decimals;
+        _initializeOwner(_owner);
+    }
+
+    /// @dev Returns the name of the token
+    function name() public view virtual override returns (string memory) {
+        return name_;
+    }
+
+    /// @dev Returns the symbol of the token
+    function symbol() public view virtual override returns (string memory) {
+        return symbol_;
+    }
+
+    /// @dev Returns the number of decimals used to get its user representation
+    function decimals() public view virtual override returns (uint8) {
+        return decimals_;
+    }
+
+    function mint(address account, uint256 amount) external onlyOwner {
+      _mint(account, amount);
+    }
+}
